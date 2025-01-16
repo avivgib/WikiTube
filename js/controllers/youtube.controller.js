dbbeedc16073ba7f505fd1176fe688ec272045b5
Aviv Gibli <avivgib1012@gmail.com>
2025-01-16 10:38:05 +0200

    Optimize API calls: add local storage handling and demo data

--- js/controllers/youtube.controller.js
+++ js/controllers/youtube.controller.js
@@ -1,6 +1,12 @@
 'use strict'
 
-// API Youtube - AIzaSyCcS-xjaZ9O97RQ0cWGvyME4-8Je_loTIE 
+const PLAYLIST_STORAGE_KEY = 'savedPlayList'
+const CURRENT_STORAGE_KEY = 'savedCurrentMovie'
+
+const MAX_API_CALLS = 1000
+let apiCallsToday = 850
+
+
 const YT_KEY = 'AIzaSyCcS-xjaZ9O97RQ0cWGvyME4-8Je_loTIE'
 const value = 5
 
@@ -15,35 +21,53 @@ function onInit() {
 
 
 function renderPlayList() {
-    // const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${YT_KEY}`
-    // console.log('Request URL:', apiUrl)
-    
-    axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${YT_KEY}`)
-    .then(res => {
-        console.log('res.data:', res.data)
-        const topSearch = res.data.items
-        console.log('res.data.items:', topSearch)
-        const elPlayList = document.querySelector('.play-list')
-        let strHTML = ''
-        
-        topSearch.forEach( song => {
-            const title = song.snippet.title
-            const pic = song.snippet.thumbnails.default.url
-            console.log('song data:', song)
-            strHTML += `
-                    <div class="play-list-card">
-                        <p>${title}</p>
-                        <img src="${pic}" alt="${title}" />
-                    </div>`
-        })
+    const topSearch = loadFromStorage(PLAYLIST_STORAGE_KEY)
 
-       innerHTML = strHTML
-    })
-    .catch(err => {
-        console.error('Error:', err.response ? err.response.data : err.message)
-    })
+    if (topSearch && topSearch.length) {
+        console.log('Data loaded from Local Storage:', topSearch)
+        renderPlayListHTML(topSearch)
+        return
+    }
+
+    if (apiCallsToday >= MAX_API_CALLS) {
+        console.log('Exceeded daily API call limit, using demo data.')
+        renderPlayListHTML(DEMO_DATA)
+        return
+    }
+
+    axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${YT_KEY}`)
+        .then(res => {
+            apiCallsToday++
+            console.log('res.data:', res.data)
+
+            const topSearch = res.data.items
+            console.log('res.data.items:', res.data.items)
+            renderPlayListHTML(topSearch)
+            saveToStorage(PLAYLIST_STORAGE_KEY, topSearch)
+        })
+        .catch(err => {
+            console.error('Error fetching from API:', err.response ? err.response.data : err.message)
+        })
 }
 
+function renderPlayListHTML(playList) {
+    const elPlayList = document.querySelector('.play-list')
+    let strHTML = ''
 
-function renderCurrentVideo() {}
-function renderWikipedia() {}
+    playList.forEach(song => {
+        const title = song.snippet.title
+        const pic = song.snippet.thumbnails.default.url
+        console.log('song data:', song)
+        
+        strHTML += `
+                <div class="play-list-card">
+                    <p>${title}</p>
+                    <img src="${pic}" alt="${title}" />
+                </div>`
+    })
+
+    elPlayList.innerHTML = strHTML
+}
+
+function renderCurrentVideo() { }
+function renderWikipedia() { }
---
+++ js/demo-data/demo.data.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const DEMO_DATA = [
+    {
+        snippet: {
+            title: "Demo Video 1",
+            thumbnails: {
+                default: {
+                    url: "https://via.placeholder.com/120x90?text=Video+1"
+                }
+            }
+        },
+    },
+    {
+        snippet: {
+            title: "Demo Video 2",
+            thumbnails: {
+                default: {
+                    url: "https://via.placeholder.com/120x90?text=Video+2"
+                }
+            }
+        },
+    },
+    {
+        snippet: {
+            title: "Demo Video 3",
+            thumbnails: {
+                default: {
+                    url: "https://via.placeholder.com/120x90?text=Video+3"
+                }
+            }
+        },
+    },
+    {
+        snippet: {
+            title: "Demo Video 4",
+            thumbnails: {
+                default: {
+                    url: "https://via.placeholder.com/120x90?text=Video+4"
+                }
+            }
+        },
+    },
+    {
+        snippet: {
+            title: "Demo Video 5",
+            thumbnails: {
+                default: {
+                    url: "https://via.placeholder.com/120x90?text=Video+5"
+                }
+            }
+        }
+    }
+];
