a8d6897f9002b4ebe0f05b36afe850a67df6a53d
Aviv Gibli <avivgib1012@gmail.com>
2025-01-20 05:10:07 +0200

    Add functions to fetch and render Wikipedia data from API

--- js/controllers/wikipedia.controller.js
+++ js/controllers/wikipedia.controller.js
@@ -1,2 +1,29 @@
 'use strict'
 
+function renderWikipedia() {
+    return getWikiData(WIKI_STORAGE_KEY)
+        .then(wikiData => {
+            renderWikiDataHTML(wikiData)
+            return wikiData
+        })
+        .catch(err => {
+            console.error(`Failed to fetch wikiData: ${err}`)
+        })
+}
+
+function renderWikiDataHTML(wikiListData) {
+    const elWikipediaData = document.querySelector('.wikipedia-section')
+    elWikipediaData.innerHTML = ''
+    console.log(`inputSearchValue: ${wikiListData}`)
+    debugger
+
+    wikiListData.map(wikiData => {
+        const { title, snippet } = wikiData
+
+        elWikipediaData.innerHTML += `
+                <div class="wiki-data">
+                    <h5>${title}</h5>
+                    <p>${snippet}</p>
+                </div>`
+    })
+}
--- js/services/wikipedia.service.js
+++ js/services/wikipedia.service.js
@@ -1,2 +1,26 @@
 'use strict'
 
+const WIKI_STORAGE_KEY = "Johnny Maestro"
+
+function getWikiData(searchKey = '') {
+    const wikiData = loadFromStorage(WIKI_STORAGE_KEY)
+
+    if (wikiData && wikiData.length) {
+        console.log('Loading data from local storage...')
+        return Promise.resolve(wikiData)
+    }
+debugger
+    const url = `https://en.wikipedia.org/w/api.php?&origin=*&action=query&list=search&srsearch=${searchKey}&format=json`;
+    console.log(`url: ${url}`)
+
+    return axios.get(url)
+        .then(res => {
+            const searchData = res.data.query.search
+            saveToStorage(searchKey, searchData)
+            return Promise.resolve(searchData)
+        })
+        .catch(err => {
+            console.error('Error fetching from API:', err.response ? err.response.data : err.message)
+            return Promise.reject(err)
+        })
+}
